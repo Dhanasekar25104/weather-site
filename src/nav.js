5d2c60a3d91bb381224a93bb3814157ef1169889
Riopradheep007 <pradheep1341@gmail.com>
2023-04-08 15:14:30 +0530

    first commit

--- src/nav.js
+++ src/nav.js
@@ -3,7 +3,7 @@ import "./nav.css"
 export default function Nav(){
     return(
       <div className="nav">
-   <img src="./image/cloudy.png"></img>
+    <img src="./image/cloudy.png" alt="cloud"></img>
      <h2>simple weather</h2>
       </div>
     )
